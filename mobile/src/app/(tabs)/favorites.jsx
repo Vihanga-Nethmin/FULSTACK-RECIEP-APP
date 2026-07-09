f070142065400db1587a3bc940bbf0b8fc2d1b36
Vihanga Nethmin <vihanganethmin4@gmail.com>
2026-07-09 20:51:37 +0530

    Setting Up tab Navigators

--- mobile/src/app/(tabs)/favorites.jsx
+++ mobile/src/app/(tabs)/favorites.jsx
@@ -1,13 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const HomeScreen = () => {
+const FavoritesScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
-      
+      <Text>FavoritesScreen</Text>
     </View>
   )
 }
 
-export default HomeScreen
+export default FavoritesScreen
